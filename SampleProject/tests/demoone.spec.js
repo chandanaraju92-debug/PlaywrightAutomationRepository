23e5b926d346f08f0177ab0f7e741efa005702ee
chandanaraju92-debug <chandanaraju92@gmail.com>
2026-02-08 07:10:51 -0500

    Intial Draft

---
+++ SampleProject/tests/demoone.spec.js
@@ -0,0 +1,14 @@
+import {test, expect} from '@playwright/test'
+test("First Playwright Testcase",function show(){
+    console.log("welcome to first test case in playwright");
+
+})
+
+test("second playwright testcases", function(){
+   console.log ("second playwright testcase")
+})
+
+
+test("Third playwright testcases",()=>{
+    console.log("weolcome to third playwright testcases")
+} )
